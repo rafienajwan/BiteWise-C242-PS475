1af732d96e3f2e366de5ee8f3f874abaf6ec340b
rafienajwan <rafie.anjasmara@gmail.com>
2024-12-08 18:23:28 +0700

    Update model loading URL to use environment variable

--- src/services/pfcModel/loadPFCModel.js
+++ src/services/pfcModel/loadPFCModel.js
@@ -2,7 +2,7 @@ const tf = require('@tensorflow/tfjs-node');
 
 async function loadPFCModel() {
     try {
-        const model = await tf.loadLayersModel("https://storage.googleapis.com/bitewise-model/PFC_tfjs/model.json");
+        const model = await tf.loadLayersModel(process.env.PFC_MODEL_URL);
         return model;
     } catch (error) {
         console.error('Error loading goals model:', error);
