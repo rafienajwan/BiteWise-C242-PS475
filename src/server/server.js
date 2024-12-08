15118274ca3122cbb2b065a4b6f1cf8b34ef12de
rafienajwan <rafie.anjasmara@gmail.com>
2024-12-08 18:12:52 +0700

    Change server port to 3000 and update model loading URL to a fixed path

--- src/server/server.js
+++ src/server/server.js
@@ -6,7 +6,7 @@ const loadPFCModel = require('../services/pfcModel/loadPFCModel');
 
 (async () => {
     const server = Hapi.server({
-        port: process.env.PORT || 8080,
+        port: process.env.PORT || 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
--- src/services/pfcModel/loadPFCModel.js
+++ src/services/pfcModel/loadPFCModel.js
@@ -2,7 +2,7 @@ const tf = require('@tensorflow/tfjs-node');
 
 async function loadPFCModel() {
     try {
-        const model = await tf.loadLayersModel(process.env.PFC_MODEL_URL);
+        const model = await tf.loadLayersModel("https://storage.googleapis.com/bitewise-model/PFC_tfjs/model.json");
         return model;
     } catch (error) {
         console.error('Error loading goals model:', error);
